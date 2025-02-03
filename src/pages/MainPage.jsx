016ef57f32c0cacd5c8ea9639baf36f57f71a4ec
wth8ee <wth8eeyv2@gmail.com>
2025-02-03 20:19:02 +0300

    smaller cells

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,32 +4,28 @@ import { LeftPanel } from "../components/LeftPanel";
 import { RightPanel } from "../components/RightPanel";
 
 export function MainPage() {
-    const lsValues = JSON.parse(localStorage.getItem("values"));
-    const lsResults = JSON.parse(localStorage.getItem("results"));
-    const [values, setValues] = useState(
-        lsValues
-            ? lsValues
-            : {
-                  cyan: null,
-                  emerald: null,
-                  lime: null,
-                  yellow: null,
-                  orange: null,
-                  red: null,
-                  black: null,
-              }
-    );
-    const [results, setResults] = useState(lsResults ? lsResults : []);
+  const lsValues = JSON.parse(localStorage.getItem("values"));
+  const lsResults = JSON.parse(localStorage.getItem("results"));
+  const [values, setValues] = useState(
+    lsValues
+      ? lsValues
+      : {
+          cyan: null,
+          emerald: null,
+          lime: null,
+          yellow: null,
+          orange: null,
+          red: null,
+          black: null,
+        }
+  );
+  const [results, setResults] = useState(lsResults ? lsResults : []);
 
-    return (
-        <div className="bg-slate-300 min-h-screen w-screen flex justify-between">
-            <LeftPanel values={values} results={results} />
-            <CentralPanel
-                results={results}
-                setResults={setResults}
-                values={values}
-            />
-            <RightPanel values={values} setValues={setValues} />
-        </div>
-    );
+  return (
+    <div className="bg-slate-300 min-h-screen w-screen flex justify-between">
+      <LeftPanel values={values} results={results} />
+      <CentralPanel results={results} setResults={setResults} values={values} />
+      <RightPanel values={values} setValues={setValues} />
+    </div>
+  );
 }
